9d412ae150e84cbdf6e85e0b176841a8e4322a39
Simon Chatteleyn <kilik2049@gmail.com>
2012-09-20 12:03:39 +0200

    learn some scala stuff, better algo. have to test

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -16,26 +16,40 @@ object Main {
    */
   def pascal(c: Int, r: Int): Int =
     if (r <= 0 || c == 1 || c >= r + 1) {
-      return 1
-    } else if (c == 2 || c == r - 1) {
-      return r
-    } else {
-      val value = pascal(c - 1, r - 1) + pascal(c + 1, r - 1)
-      return value
+      1
+    } else { 
+	      if (c == 2 || c == r - 1) {
+	        r
+	    } else {
+	      val value = pascal(c - 1, r - 1) + pascal(c + 1, r - 1)
+	        value
+	    }
     }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean =
-    if (!chars.isEmpty) {
-      val l = chars.head
-      val list = chars.tail
-      balance(list)
+  def balance(chars: List[Char]): Boolean = {
+    def parenthesis(chars: List[Char], o: Int, f: Int): Boolean = {
+	    if (!chars.isEmpty) {
+	      val l = chars.head
+	      val list = chars.tail
+	        if (l == '(') {
+	          parenthesis(list, o+1, f)
+	        } else {
+	          if (l == ')') {
+	            if (f == o) false else parenthesis(list, o, f+1)
+	          } else {
+		        parenthesis(list, o, f)
+		      }
+	        }
+	    } else {
+	        (o == f) 
+	    }
     }
-    else {
-      return true
-    }
+  parenthesis(chars, 0, 0)
+  }
+  
 
   /**
    * Exercise 3
